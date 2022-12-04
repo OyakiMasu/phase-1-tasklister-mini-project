bf5bb0740507f5572a48b174b481ccc7cf0fbd58
OyakMasu21 <akzaki17@gmail.com>
2022-12-04 14:20:18 +0300

    Still in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+     buildToDo(e.target.new-task-description)
+     form.reset()
+  })
 });
+
+function buildToDo(form){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${create-task-form}`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector("#tasks").appendChild(p)
+}
+function handleDelete(e){
+e.target.parentNode.remove()
+
+}
